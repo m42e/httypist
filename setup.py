cf5e47253db2daf66ed43d2565a0a44c54797c19
Matthias Bilger <matthias@bilger.info>
2019-11-25 11:00:18 +0100

    fixed requirements and license

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from distutils.core import setup
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
 
 setup(
     name='Httypist',
     version='0.1',
     packages=['httypist',],
-    license='GPLv2',
+    license='MIT',
+    install_requires=requirements,
     long_description=open('README.md').read(),
 )
