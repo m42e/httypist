7876db34bfbdc9876a28bee08b1c3dab18081c64
Matthias Bilger <matthias@bilger.info>
2020-01-23 14:05:48 +0100

    Preparation for tests using tox

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
+import os
+import setuptools
 from distutils.core import setup
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
 
-setup(
-    name='Httypist',
-    version='0.1',
-    packages=['httypist',],
-    license='MIT',
-    install_requires=requirements,
-    long_description=open('README.md').read(),
+root = os.path.dirname(os.path.abspath(__file__))
+
+setuptools.setup(
+    name="Httypist",
+    version="0.1",
+    packages=["httypist",],
+    license="MIT",
+    install_requires=["flask", "pyyaml", "requests", "celery", "dulwich", ],
+    long_description=open(os.path.join(root, "README.md")).read(),
 )
